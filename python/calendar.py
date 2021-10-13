bdc9e02f2913b44237733a0f4648172ad5f05ac9
unknown <apurvajena@gmail.com>
2021-10-13 20:39:30 +0530

    calender

---
+++ python/calendar.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import calendar
+year =int( input("Enter the year of the required calendar "))
+month = int( input("Enter the month of the required calendar "))
+print(calendar.month(year,month)) 
+
+
+# In[ ]:
+
+
+
+
