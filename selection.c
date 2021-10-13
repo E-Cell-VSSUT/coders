a2806c1ac34143bf3437b7bb472b50437980c3cc
adnaan-ansari <78425049+adnaan-ansari@users.noreply.github.com>
2021-10-13 22:03:04 +0530

    Add files via upload

---
+++ selection.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+   int arr[6]={6,12,0,18,11,99};
+   int n=6;
+   int i, j, position, swap;
+   for (i = 0; i < (n - 1); i++) {
+      position = i;
+      for (j = i + 1; j < n; j++) {
+         if (arr[position] > arr[j])
+            position = j;
+      }
+      if (position != i) {
+         swap = arr[i];
+         arr[i] = arr[position];
+         arr[position] = swap;
+      }
+   }
+   for (i = 0; i < n; i++)
+      printf("%d ", arr[i]);
+   return 0;
+}
